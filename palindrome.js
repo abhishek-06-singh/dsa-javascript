70d22d2cdf5862ed198af2b32458534e030abe29
Abhishek singh chauhan <avi7000201383@gmail.com>
2024-05-30 20:54:35 +0530

    added Palindrome number question

---
+++ palindrome.js
@@ -0,0 +1,19 @@
+//! Write a function that checks if a given number is a palindrome. A palindrome is a string that reads the same forward and backward ?
+
+// ! Input: x =121;
+// ! Output: True;
+
+// ! Input: x = 534;
+// ! Output: False;
+
+const isPalindrome = (x) => {
+  if (x < 0) return false;
+  if (x < 10) return true;
+  return x === +x.toString().split("").reverse().join("");
+};
+const res = isPalindrome(0);
+console.log(res);
+
+// * toString convert number to string example = 121 => "121"
+
+// * .split convert string to array and split example = "121" => ["1", "2", "3"]
